9365485895a3cc2d8f60e8a272ca94e635a42841
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2023-09-12 14:59:23 +0700

    Update time.js

--- js/time.js
+++ js/time.js
@@ -4,20 +4,20 @@ const clockMeridiem = clock.querySelector(".js-clock__am-pm");
 
 const date = document.querySelector(".js-home__date");
 
-const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const days = ["CN", "T2", "T3", "T4", "T5", "T6", "T7"];
 const months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
+  "Tháng 1",
+  "Tháng 2",
+  "Tháng 3",
+  "Tháng 4",
+  "Tháng 5",
+  "Tháng 6",
+  "Tháng 7",
+  "Tháng 8",
+  "Tháng 9",
+  "Tháng 10",
+  "Tháng 11",
+  "Tháng 12",
 ];
 
 const RED_TEXT_CLASSNAME = "red-text";
