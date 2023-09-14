e24c133bc407726869a25a2df97092ff79a79120
thanhdatio <83289656+thanhdatio@users.noreply.github.com>
2023-09-14 10:31:13 +0700

    Update global.js

--- js/global.js
+++ js/global.js
@@ -12,10 +12,12 @@ const TO_DO_SCREEN = document.querySelector(".js-background__to-do");
 const TO_DO_DATE = TO_DO_SCREEN.querySelector(".js-titles__header h3");
 
 const TOOLS_SCREEN = document.querySelector(".js-background__tools");
-const TOOLS_DATE = TOOLS_SCREEN.querySelector(".js-titles__header h3");
+//const TOOLS_DATE = TOOLS_SCREEN.querySelector(".js-titles__header h3");
 
 const WEB_SCREEN = document.querySelector(".js-background__web");
-const WEB_DATE = WEB_SCREEN.querySelector(".js-titles__header h3");
+//const WEB_DATE = WEB_SCREEN.querySelector(".js-titles__header h3");
+
+const RELAX_SCREEN = document.querySelector(".js-background__relax");
 
 const CHECKED_CLASSNAME = "checked";
 const CLICK_EVENT = "click";
